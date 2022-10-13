7d202ff9be56d78af7a95d932db92e2d58d5974b
CamilaGon <cg561351@gmail.com>
2022-10-13 19:56:38 -0300

    retorno

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,5 @@
-const fetchProducts = async (produto) => {
-  try {
-    const fullUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${produto}`;
-    const response = await fetch(fullUrl);
-    const dataBase = await response.json();
-    return dataBase;
-  } catch (error) {
-    throw new Error('You must provide an url');
-  } 
+const fetchProducts = async () => {
+
 };
 
 if (typeof module !== 'undefined') { 
