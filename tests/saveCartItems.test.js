80320703cb1f5b839f57fd83c602ad3efb5e40a2
CamilaGon <cg561351@gmail.com>
2022-10-21 15:37:13 -0300

    requisito

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,7 +5,7 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
-    saveCartItems('cardItem');
+    saveCartItems('cartItem');
     expect(localStorage.setItem).toHaveBeenCalled();
   });
   it('Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
